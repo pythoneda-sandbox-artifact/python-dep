30d38c6d94bdb76d6299300b838ae10212ebd1d7
UnveilingPartner <github.bot@acm-sl.org>
2023-11-06 16:14:56 +0100

    New tag 0.0.85 in https://github.com/pythoneda-sandbox/python-dep

--- python-dep/flake.nix
+++ python-dep/flake.nix
@@ -47,7 +47,7 @@
       let
         org = "pythoneda-sandbox";
         repo = "python-dep";
-        version = "0.0.84";
+        version = "0.0.85";
         sha256 = "0knfknlyyjs41smhm01dy2h1nmx5s5dcypakpnz6aa3bagxz812z";
         pname = "${org}-${repo}";
         pkgs = import nixos { inherit system; };
